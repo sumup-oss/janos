b76ba9413f435289d042b9fb45563c49858caad3
Pablo Loschi <loschi.pablo@gmail.com>
2021-03-03 13:23:47 +0100

    Revert "add foundry checks"
    This reverts commit f6142c215178ca86b0b680144bec832e57cba0e0.

--- src/app.js
+++ src/app.js
@@ -1,36 +1,21 @@
-/**
- * Copyright 2021, SumUp Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #!/usr/bin/env node
 
-const glob = require('glob');
-const argv = require('minimist')(process.argv.slice(2));
+const glob = require("glob");
+const argv = require("minimist")(process.argv.slice(2));
 
-const yaml = require('./yaml');
-const migration = require('./migration');
-const cli = require('./cli');
+const yaml = require("./yaml");
+const migration = require("./migration");
+const cli = require("./cli");
 
 function main() {
   try {
-    console.log('Starting script...');
+    console.log("Starting script...");
     const args = cli.parseArgs(argv);
     if (args.file) {
-      console.log('Converting file:', args.file);
+      console.log("Converting file:", args.file);
       convertFile(args.file);
     } else if (args.dir) {
-      glob(`${args.dir}/**/*.y[a]ml`, {}, (err, files) => {
+      glob(`${args.dir}/**/*.y[a]ml`, {}, function (err, files) {
         files.forEach((file) => {
           console.log("Converting file:", file);
           convertFile(file);
@@ -38,7 +23,7 @@ function main() {
       });
     }
   } catch (e) {
-    console.log('Error:', e);
+    console.log("Error:", e);
   }
 }
 
@@ -49,7 +34,7 @@ function convertFile(path) {
     yaml.writeFile(path, result);
   } catch (e) {
     console.log(e);
-    console.log('Could not read file', path);
+    console.log("Could not read file", path);
   }
 }
 
